a9dd7fac19a535b3472594b3422895ef2c3c6bce
SumeetSaraf <Sumeet.Saraf@kibocommerce.com>
2021-12-10 22:18:44 +0530

    SQL Connection String and DB migration

---
+++ Migrations/20211210164624_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InventoryApiApp.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "dbo");
+
+            migrationBuilder.CreateTable(
+                name: "Inventory",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<int>(type: "int", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Inventory", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Inventory",
+                schema: "dbo");
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,8 +1,15 @@
+using InventoryApiApp.Data;
+using InventoryApiApp.Interface;
+using InventoryApiApp.Models;
+using InventoryApiApp.Repository;
+using InventoryApiApp.Service;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace InventoryApiApp
 {
@@ -18,8 +25,17 @@ namespace InventoryApiApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
-            services.AddSwaggerGen();
+            services.AddHttpClient();
+            services.AddTransient<IRepository<Inventory>, RepositoryInventory>();
+            services.AddTransient<InventoryService, InventoryService>();
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CRUDAspNetCore5WebAPI", Version = "v1" });
+            });
+            //services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,6 +44,8 @@ namespace InventoryApiApp
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CRUDAspNetCore5WebAPI v1"));
             }
 
             app.UseHttpsRedirection();
@@ -41,16 +59,16 @@ namespace InventoryApiApp
                 endpoints.MapControllers();
             });
 
-            // if (app.Environment.IsDevelopment())
-            // {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-            // }
-            app.UseSwaggerUI(options =>
-            {
-                options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-                options.RoutePrefix = string.Empty;
-            });
+            //// if (app.Environment.IsDevelopment())
+            //// {
+            //app.UseSwagger();
+            //app.UseSwaggerUI();
+            //// }
+            //app.UseSwaggerUI(options =>
+            //{
+            //    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            //    options.RoutePrefix = string.Empty;
+            //});
         }
     }
 }
